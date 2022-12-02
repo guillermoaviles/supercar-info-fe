cf06942a4a4080886e98d39d85ed7c355b8387a0
nahmad94 <deevan11@gmail.com>
2022-12-02 16:02:33 -0500

    sets up search component

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import Header from './Header';
+import Search from './Search';
+import Gallery from './Gallery';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <h1>Welcome to SuperCars!</h1>
+      <Search />
+      <Gallery />
     </div>
   );
 }
