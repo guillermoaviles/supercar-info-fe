e66ff3855d8d9a2b62b7ec8c136d7c6c675cf7bf
nahmad94 <deevan11@gmail.com>
2022-12-04 13:48:33 -0500

    stuff

--- src/Components/App.js
+++ src/Components/App.js
@@ -3,8 +3,20 @@ import Header from './Header';
 import Search from './Search';
 import Gallery from './Gallery';
 import Footer from './Footer';
+import { useEffect, useState } from 'react'
+import axios from 'axios';
 
 function App() {
+  const api = `http://localhost:8000/api/getsupercars/`
+  let [data, setData] = useState([])
+  useEffect(() => {
+    axios.get(api)
+  })
+  .then((res) => res.json())
+  .then((json) => {
+    setData(json)
+  })
+  console.log(data)
   return (
     <div className="App">
       <Header />
