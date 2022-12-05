f6ce5beaa99aac4a5d73ce789feb7e831932edd9
username <gjaviles7@gmail.com>
2022-12-04 22:31:32 -0500

    demo routes

--- src/Components/App.js
+++ src/Components/App.js
@@ -4,14 +4,27 @@ import Search from './Search';
 import Gallery from './Gallery';
 import Footer from './Footer';
 import Cars from './Car';
+import { Link, Route, Routes } from 'react-router-dom';
+import { useState } from "react";
 
 function App() {
+
+  const [make, setMake] = useState({make: ''});
+  const [model, setModel] = useState({model: ''});
+  const [year, setYear] = useState({year: ''});
+  const [transmission, setTransmission] = useState({transmission: ''});
+  const [engine, setEngine] = useState({engine: ''});
+  const [price, setPrice] = useState({price: ''});
+  const [mileage, setMileage] = useState({mileage: ''});
+
   return (
     <div className="App">
       <Header />
-      <Search />
-      <Cars />
-      {/* <Gallery /> */}
+      <Routes>
+        <Route path='/' element={<Gallery />}/>
+        <Route path='/search' element={<Search setMake = {setMake} setModel={setModel} setYear={setYear} setTransmission={setTransmission} setEngine={setEngine} setPrice={setPrice} setMileage={setMileage}/>}/>
+        <Route path='/cars' element={<Cars make = {make} model = {model} year = {year} transmission = {transmission} engine = {engine} price = {price} mileage = {mileage}/>}/>
+      </Routes>
       <Footer />
     </div>
   );
--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,55 +1,49 @@
 import React from 'react'
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
-export default function Search() {
+export default function Search({setMake, setModel, setYear, setTransmission, setEngine, setPrice, setMileage}) {
 
-  const [make, setMake] = useState({make: ''});
-  const [model, setModel] = useState({model: ''});
-  const [year, setYear] = useState({year: ''});
-  const [transmission, setTransmission] = useState({transmission: ''});
-  const [engine, setEngine] = useState({engine: ''});
-  const [price, setPrice] = useState({price: ''});
-  const [mileage, setMileage] = useState({mileage: ''});
 
   const handleMake = (event) => {
     setMake({
-      make: event.target.value
+      make: event.target.document.getElementById("make").value
     })
   }
 
   const handleModel = (event) => {
     setModel({
-      model: event.target.value
+      model: event.target.document.getElementById("model").value
       })
   }
 
   const handleYear = (event) => {
     setYear({
-      year: event.target.value
+      year: event.target.document.getElementById("year").value
       })
   }
 
   const handleTransmission = (event) => {
     setTransmission({
-      transmission: event.target.value
+      transmission: event.target.document.getElementById("transmission").value
       })
   }
 
   const handleEngine = (event) => {
     setEngine({
-      engine: event.target.value
+      engine: event.target.document.getElementById("engine").value
       })
   }
 
   const handlePrice = (event) => {
     setPrice({
-      price: event.target.value
+      price: event.target.document.getElementById("price").value
       })
   }
 
   const handleMileage = (event) => {
     setMileage({
-      mileage: event.target.value
+      mileage: event.target.document.getElementById("mileage").value
       })
   }
 
@@ -59,48 +53,62 @@ export default function Search() {
         <h1>Welcome to SuperCars!</h1>
         <h2>Looking for something specific? Search below:</h2>
         <div>
-          <input type="text" id="make" placeholder='make'></input>
-          <button type='submit' onClick={handleMake}>Search</button>
+          <input type="text" id="make" placeholder='make' onChange={handleMake}></input>
+          <Link to = '/car'>
+                <h1>Search</h1>
+          </Link>
         </div>
         <div>
-          <input type="text" id="model" placeholder='model'></input>
-          <button type='submit'  onClick={handleModel}>Search</button>
+          <input type="text" id="model" placeholder='model' onChange={handleModel}></input>
+          <Link to = '/car'>
+                <h1>Search</h1>
+          </Link>
         </div>
         <div>
-          <input type="text" id="year" placeholder='year'></input>
-          <button type='submit' onClick={handleYear}>Search</button>
+          <input type="text" id="year" placeholder='year' onChange={handleYear}></input>
+          <Link to = '/car'>
+                <h1>Search</h1>
+          </Link>
         </div>
         <div>
-          <input type="text" id="engine" placeholder='engine'></input>
-          <button type='submit' onClick={handleEngine}>Search</button>
+          <input type="text" id="engine" placeholder='engine' onChange={handleEngine}></input>
+          <Link to = '/car'>
+                <h1>Search</h1>
+          </Link>
         </div>
        <div>
-        <label for="transmission">Transmission:</label>
-            <select name="transmission" id="transmission">
+        <label htmlFor="transmission">Transmission:</label>
+            <select name="transmission" id="transmission" onChange={handleTransmission}>
               <option value="automatic">Automatic</option>
               <option value="manual">Manual</option>
               </select>
-        <button type='submit' onClick={handleTransmission}>Search</button>
+        <Link to = '/car'>
+                <h1>Search</h1>
+        </Link>
        </div>
        <div>
-        <label for="price">Price range:</label>
-            <select name="price" id="price">
+        <label htmlFor="price">Price range:</label>
+            <select name="price" id="price" onChange={handlePrice}>
               <option value="lessThan75">less than $75,000</option>
               <option value="75to150">$75,000 - $150,000</option>
               <option value="150to250">$150,000 - $250,000</option>
               <option value="250up">$250,000 +</option>
               </select>
-          <button type='submit' onClick={handlePrice}>Search</button>
+        <Link to = '/car'>
+                <h1>Search</h1>
+        </Link>
        </div>
       <div>
-        <label for="mileage">Mileage:</label>
-            <select name="mileage" id="mileage">
+        <label htmlFor="mileage">Mileage:</label>
+            <select name="mileage" id="mileage" onChange={handleMileage}>
               <option value="lessThan20">less than 20,000</option>
               <option value="21to50">21,000 - 50,000</option>
               <option value="51to80">51,000 - 80,000</option>
               <option value="81up">81,000 +</option>
               </select>
-        <button type='submit' onClick={handleMileage}>Search</button>
+        <Link to = '/cars'>
+                <h1>Search</h1>
+        </Link>
       </div>
     </div>
   )
