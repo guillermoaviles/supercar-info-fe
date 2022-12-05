887891ab38c2dc74e4cb090571ded57780e84fe6
nahmad94 <deevan11@gmail.com>
2022-12-05 12:21:26 -0500

    EOD

--- src/Components/App.js
+++ src/Components/App.js
@@ -4,14 +4,43 @@ import Search from './Search';
 import Gallery from './Gallery';
 import Footer from './Footer';
 import Cars from './Car';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import {Link, Route, Routes} from 'react-router-dom'
 
 function App() {
+  const [cars, setCars] = useState([]); 
+
+    useEffect(() => {
+        axios.get('http://localhost:8000/api/getsupercars/')
+          .then((response) => response.data)
+          .then((response) => {
+            console.log(response)
+            setCars(response) 
+          })
+    }, []);
+    console.log(cars)
   return (
     <div className="App">
-      <Header />
-      <Search />
+      <nav>
+        <Link to="/">
+          <Header />
+        </Link>
+        {/* <Link to="/search">
+          <Search />
+        </Link> */}
+      </nav>
+      <main>
+        {/* <Routes> */}
+          {/* <Route path='/' element={ <Header /> }/> */}
+          <Route path='/' element={ <Gallery cars={cars} setCars={cars} />} />
+          {/* <Route path='/cars/:_id' element={ <Cars cars={cars} setCars={cars} />} /> */}
+          {/* <Route path='/search' element={ <Search />} /> */}
+        {/* </Routes> */}
+      </main>
+
       <Cars />
-      {/* <Gallery /> */}
+      <Gallery />
       <Footer />
     </div>
   );
--- src/Components/Car.js
+++ src/Components/Car.js
@@ -3,21 +3,11 @@ import { useState } from "react";
 import { useEffect } from 'react';
 import axios from 'axios';
 
-export default function Cars() {
-    const [cars, setCars] = useState([]); 
-
-    useEffect(() => {
-        axios.get('http://localhost:8000/api/getsupercars')
-          .then((response) => response.data)
-          .then((response) => {
-            console.log(response)
-            setCars(response) 
-          })
-    }, []);
-
+export default function Cars(props) {
+    
     return (
         <div>
-            {cars.map((element) => {
+            {props.cars.map((element) => {
                 return( 
                     <div id={element._id}>
                         <img src={element.image}/>
@@ -33,4 +23,4 @@ export default function Cars() {
             })}
         </div>
     )
-}
+} 
